ebc4a95be3ab6b3dff24feb9a9f0ffd28b6af769
luckyamina <144383353+luckyamina@users.noreply.github.com>
2023-11-03 19:35:45 +0400

    Add files via upload

---
+++ leasson5.py
@@ -0,0 +1,17 @@
+# age difference 
+# User_age = int(input("Your age: "))
+# UFathers_age = int(input("Your fathers age: "))
+# Year = 2023
+# Age_difference = UFathers_age - User_age
+
+# for i in range(10):
+#     print("In", Year, "Difference between your age and your fathers age will be", Age_difference)
+#     Year += 1
+#     Age_difference += 1
+
+# ლუწი და კენტი რიცხვები 30 ის ჩათვლით
+num = 0
+print(num)
+while num < 30:
+    num += 3
+    print(num)
